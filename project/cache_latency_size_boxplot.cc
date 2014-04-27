01cc223ec3f5c56949016423d05247861aedd509
root <root@menglab6c.(none)>
2014-04-27 10:47:14 -0400

    cache monitor

--- project/cache_latency_size_boxplot.cc
+++ project/cache_latency_size_boxplot.cc
@@ -22,16 +22,21 @@ static __inline__ unsigned long long rdtsc(void)
 }
 #endif
 
-#define CACHE_LINE_SIZE	64
+#define CACHE_LINE_SIZE 64
 
-#define WSS 24567 /* 24 Mb */
-#define NUM_VARS WSS * 1024 / sizeof(long)
 
 // ./a.out memsize(in KB)
 int main(int argc, char** argv)
 {
-	unsigned long mem_size_KB = atol(argv[1]);  // mem size in KB
-	unsigned long mem_size_B  = mem_size_KB * 1024;	// mem size in Byte
+    if( argc < 4 ) {
+        printf("[usage] ./program mem_size_KB iteration sleep_utime\n");
+        exit(1);
+    }
+
+    unsigned long mem_size_KB = atol(argv[1]);  // mem size in KB
+    unsigned long iteration = atol(argv[2]);
+    unsigned long sleep_utime = atol(argv[3]);
+    unsigned long mem_size_B  = mem_size_KB * 1024; // mem size in Byte
     unsigned long count       = mem_size_B / sizeof(long);
     unsigned long row         = mem_size_B / CACHE_LINE_SIZE;
     int           col         = CACHE_LINE_SIZE / sizeof(long);
@@ -39,40 +44,48 @@ int main(int argc, char** argv)
     unsigned long long start, finish, dur1;
     unsigned long temp;
 
+    if( iteration > row ){
+        printf("Array row size is smaller than the iteration. Cannot record all data. exit\n");
+        exit(1);
+    }
+
     long *buffer;
     buffer = new long[count];
 
     // init array
-    for (unsigned long i = 0; i < count; ++i)
-        buffer[i] = i;
-
-    for (unsigned long i = row-1; i >0; --i) {
-        temp = rand()%i;
-        swap(buffer[i*col], buffer[temp*col]);
-    }
+    for(unsigned long i = 0; i < row; ++i)
+        for( int j = 0; j < col; ++j)
+                buffer[i*col+j] = i;
 
     // warm the cache again
-    temp = buffer[0];
-    for (unsigned long i = 0; i < row-1; ++i) {
-        temp = buffer[temp];
+    for(unsigned long i = 0; i < row; ++i) {
+//            printf("%ld \t", i);
+        for( int j = 0; j < col; ++j) {
+            temp = buffer[i*col+j];
+//            printf("%ld ", temp);
+        }
+//        printf("\n");
     }
 
-    // should be cache hit
-    temp = buffer[0];
-    start = rdtsc();
-    int sum = 0;
-    for (unsigned long i = 0; i < row-1; ++i) {
-        if (i%2 == 0) sum += buffer[temp];
-        else sum -= buffer[temp];
-        temp = buffer[temp];
+    for( unsigned long k = 0; k < iteration; ++k ) {
+        start = rdtsc();
+        for( unsigned long i = 0; i < row; i+=5 ) {
+            buffer[i*col] = buffer[i*col] * 2 - i; /*be careful not to write our time record*/
+        }
+        finish = rdtsc();
+        //record teh duration in the second long of each cache line
+        buffer[k*col+2] = finish - start;
+        if( sleep_utime > 0 ) {
+            usleep(sleep_utime);
+        }
     }
-    finish = rdtsc();
-    dur1 = finish-start;
 
-    // Res
-	printf("%lld %lld\n", dur1, dur1/row);
+    /* print out the time latency of each iteration */
+    for ( unsigned long k = 0; k < iteration; ++k ){
+        printf("%ld %ld\n", k, buffer[k*col+2]);
+    }
 
     free(buffer);
 
-	return 0;
+    return 0;
 }
